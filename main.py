3a80302efaf05e41ebe528c29ab05795ff4ba991
Abdullah <73560425+zer-far@users.noreply.github.com>
2020-12-01 22:31:01 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+forename = "Abdullah"
+surname = "Farid"
+
+# print("My first name is {0} and my last name is {1}.".format(forename, surname))
+
+# print(f"My first name is {forename} and my last name is {surname}.")
+
+# print(f"{forename} {surname} is my full name")
+
+# python = True
+# html = False
+# nothing = None
+
+# age = 5
+# if age == 18:
+#     print("You are 18.")
+# else:
+#     print("You are not 18.")
+
+a = 1
+b = 2
+print("bigger" if a > b else "smaller")
+
+age = 13
+if age > 16:
+    print("Welcome")
+else:
+    print("Try later")
+    
